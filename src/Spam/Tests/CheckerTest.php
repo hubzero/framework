d029f45864ee7e3475155f48913185d4cbc1dfd1
Shawn Rice <zooley@purdue.edu>
2016-06-27 11:12:15 +0000

    Adding tests for abstract (spam detector) Service class and fixing docblock

--- src/Spam/Tests/CheckerTest.php
+++ src/Spam/Tests/CheckerTest.php
@@ -22,7 +22,7 @@
  *
  * HUBzero is a registered trademark of Purdue University.
  *
- * @package   hubzero-cms
+ * @package   framework
  * @copyright Copyright 2005-2015 HUBzero Foundation, LLC.
  * @license   http://www.gnu.org/licenses/lgpl-3.0.html LGPLv3
  */
