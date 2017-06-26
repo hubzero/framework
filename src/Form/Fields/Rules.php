b6779f2f9f9e2e4e8b78f6e805b427ec647f7f4f
Patrick Mulligan <jpmulligan@purdue.edu>
2017-06-26 15:48:10 -0400

    [Permissions] Add a few tweaks/fixes to get permissions set on an individiual item within a component working.

--- src/Form/Fields/Rules.php
+++ src/Form/Fields/Rules.php
@@ -67,10 +67,12 @@ class Rules extends Field
 		$assetField = $this->element['asset_field'] ? (string) $this->element['asset_field'] : 'asset_id';
 
 		// Get the actions for the asset.
-		$component = $component ? App::get('component')->path($component) . '/config/access.xml' : '';
-		$section   = $section ? "/access/section[@name='" . $section . "']/" : '';
-
-		$actions = Access::getActionsFromFile($component, $section);
+		$ruleFile = Component::path($component) . '/config/access.xml';
+		$actions = Access::getActionsFromFile($ruleFile, "/access/section[@name='" . $section . "']/");
+		if (empty($actions))
+		{
+			$actions = array();
+		}
 
 		// Iterate over the children and add to the actions.
 		foreach ($this->element->children() as $el)
@@ -128,6 +130,8 @@ class Rules extends Field
 		$html[] = '<div id="permissions-sliders" class="pane-sliders">';
 		$html[] = '<p class="rule-desc">' . $lang->txt('JLIB_RULES_SETTINGS_DESC') . '</p>';
 		$html[] = '<div id="permissions-rules">';
+		// If AssetId is blank and section wasn't set to component, set it to the component name here for inheritance checks.
+		$assetId = empty($assetId) && $section != 'component' ? $component : $assetId;
 
 		// Start a row for each user group.
 		foreach ($groups as $group)
@@ -179,7 +183,6 @@ class Rules extends Field
 				$html[] = '<select name="' . $this->name . '[' . $action->name . '][' . $group->value . ']" id="' . $this->id . '_' . $action->name
 					. '_' . $group->value . '" title="'
 					. $lang->txt('JLIB_RULES_SELECT_ALLOW_DENY_GROUP', $lang->txt($action->title), trim($group->text)) . '">';
-
 				$inheritedRule = Access::checkGroup($group->value, $action->name, $assetId);
 
 				// Get the actual setting for the action for this group.
