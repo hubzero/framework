d22155b9c7435de7ae2521c0772e36afdd26feb4
Shawn Rice <zooley@purdue.edu>
2018-04-20 11:15:34 -0400

    [refactor][feat] Reworked classes to be ORM models (#424)
    * [refactor][feat] Reworked classes to be ORM models
    Also adding a model to represent a table for capturing data for each
    user/authenticator combination.
    
    * [style] Formatting fixes
    
    * [style] Docblock fixes

--- src/Auth/Domain.php
+++ src/Auth/Domain.php
@@ -31,104 +31,167 @@
 
 namespace Hubzero\Auth;
 
+use Hubzero\Database\Relational;
+use Date;
+
 /**
- * Authentication domain
+ * Authentication Domain
  */
-class Domain
+class Domain extends Relational
 {
 	/**
-	 * Record ID
-	 *
-	 * @var  integer
-	 */
-	private $id;
-
-	/**
-	 * Domain type
+	 * The table namespace
 	 *
 	 * @var  string
 	 */
-	private $type;
+	protected $namespace = 'auth';
 
 	/**
-	 * Authenticator name
+	 * The table to which the class pertains
+	 *
+	 * This will default to #__{namespace}_{modelName} unless otherwise
+	 * overwritten by a given subclass. Definition of this property likely
+	 * indicates some derivation from standard naming conventions.
 	 *
 	 * @var  string
 	 */
-	private $authenticator;
+	protected $table = '#__auth_domain';
 
 	/**
-	 * Domain name
+	 * Default order by for model
 	 *
 	 * @var  string
 	 */
-	private $domain;
+	public $orderBy = 'id';
 
 	/**
-	 * Parameters
+	 * Default order direction for select queries
 	 *
 	 * @var  string
 	 */
-	private $params;
+	public $orderDir = 'asc';
 
 	/**
-	 * List of updated keys
+	 * Fields and their validation criteria
 	 *
 	 * @var  array
 	 */
-	private $_updatedkeys = array();
+	protected $rules = array(
+		'authenticator' => 'notempty'
+	);
 
 	/**
-	 * Update all fields?
+	 * Automatically fillable fields
 	 *
-	 * @var  boolean
-	 */
-	private $_updateAll = false;
+	 * @var  array
+	 **/
+	public $always = array(
+		'type'
+	);
 
 	/**
-	 * Constructor
+	 * Generates automatic authenticator field value
 	 *
-	 * @return  void
+	 * @param   array   $data  the data being saved
+	 * @return  string
 	 */
-	public function __construct()
+	public function automaticAuthenticator($data)
 	{
-	}
-
-	/**
-	 * Clear any existing data
-	 *
-	 * @return  void
-	 */
-	public function clear()
-	{
-		$cvars = get_class_vars(__CLASS__);
-
-		$this->_updatedkeys = array();
-
-		foreach ($cvars as $key => $value)
+		$alias = $data['authenticator'];
+		$alias = strip_tags($alias);
+		$alias = trim($alias);
+		if (strlen($alias) > 255)
 		{
-			if ($key{0} != '_')
-			{
-				unset($this->$key);
-
-				$this->$key = null;
-			}
+			$alias = substr($alias . ' ', 0, 255);
+			$alias = substr($alias, 0, strrpos($alias, ' '));
 		}
 
-		$this->_updateAll = false;
-		$this->_updatedkeys = array();
+		return preg_replace("/[^a-zA-Z0-9]/", '', strtolower($alias));
 	}
 
 	/**
-	 * Log a debug message
+	 * Generates automatic modified field value
 	 *
-	 * @param   string  $msg  Message to log
-	 * @return  void
+	 * @param   array   $data  the data being saved
+	 * @return  string
 	 */
-	private function logDebug($msg)
+	public function automaticType($data)
 	{
-		$xlog = \App::get('log')->logger('debug');
-		$xlog->debug($msg);
+		return (isset($data['type']) && $data['type'] ? $data['type'] : 'authentication');
+	}
+
+	/**
+	 * Get associated links
+	 *
+	 * @return  object
+	 */
+	public function links()
+	{
+		return $this->oneToMany(__NAMESPACE__ . '\\Link', 'auth_domain_id');
+	}
+
+	/**
+	 * Read a record
+	 *
+	 * @return  boolean  True on success, False on failure
+	 */
+	public function read()
+	{
+		if ($this->get('id'))
+		{
+			$row = self::oneOrNew($this->get('id'));
+		}
+		else
+		{
+			$row = self::all()
+				->whereEquals('type', $this->get('type'))
+				->whereEquals('authenticator', $this->get('authenticator'))
+				->whereEquals('domain', $this->get('domain'))
+				->row();
+		}
+
+		if (!$row || !$row->get('id'))
+		{
+			return false;
+		}
+
+		foreach (array_keys($this->getAttributes()) as $key)
+		{
+			$this->set($key, $row->get($key));
+		}
+
+		return true;
+	}
+
+	/**
+	 * Create a record
+	 *
+	 * @return  boolean  True on success, False on failure
+	 */
+	public function create()
+	{
+		return $this->save();
+	}
+
+	/**
+	 * Update a record
+	 *
+	 * @param   boolean  $all  Update all properties?
+	 * @return  boolean
+	 */
+	public function update($all = false)
+	{
+		return $this->save();
+	}
+
+	/**
+	 * Delete a record
+	 *
+	 * @return  boolean
+	 */
+	public function delete()
+	{
+		return $this->destroy();
 	}
 
 	/**
@@ -141,38 +204,21 @@ class Domain
 	 */
 	public static function getInstance($type, $authenticator, $domain)
 	{
-		$hzad = new self();
-		$hzad->type = $type;
-		$hzad->authenticator = $authenticator;
-		$hzad->domain = $domain;
-		$hzad->read();
+		$query = self::all()
+			->whereEquals('type', $type)
+			->whereEquals('authenticator', $authenticator);
+		if ($domain)
+		{
+			$query->whereEquals('domain', $domain);
+		}
+		$row = $query->row();
 
-		if (!$hzad->id)
+		if (!$row || !$row->get('id'))
 		{
 			return false;
 		}
 
-		return $hzad;
-	}
-
-	/**
-	 * Find a record by ID
-	 *
-	 * @param   integer  $id
-	 * @return  mixed    Object on success, False on failure
-	 */
-	public static function find_by_id($id)
-	{
-		$hzad = new self();
-		$hzad->id = $id;
-		$hzad->read();
-
-		if (empty($hzad->authenticator))
-		{
-			return false;
-		}
-
-		return $hzad;
+		return $row;
 	}
 
 	/**
@@ -183,394 +229,46 @@ class Domain
 	 * @param   string  $domain
 	 * @return  mixed
 	 */
-	public function createInstance($type, $authenticator, $domain = null)
+	public static function createInstance($type, $authenticator, $domain = null)
 	{
 		if (empty($type) || empty($authenticator))
 		{
 			return false;
 		}
 
-		$instance = new self();
-		$instance->type = $type;
-		$instance->authenticator = $authenticator;
-		$instance->domain = $domain;
-		$instance->create();
+		$row = self::blank();
+		$row->set('type', $type);
+		$row->set('authenticator', $authenticator);
+		if ($domain)
+		{
+			$row->set('domain', $domain);
+		}
+		$row->save();
 
-		if (!$instance->id)
+		if (!$row->get('id'))
 		{
 			return false;
 		}
 
-		return $instance;
+		return $row;
 	}
 
 	/**
-	 * Create a record
+	 * Find a record by ID
 	 *
-	 * @return  boolean  True on success, False on failure
+	 * @param   integer  $id
+	 * @return  mixed    Object on success, False on failure
 	 */
-	public function create()
+	public static function find_by_id($id)
 	{
-		$db =  \App::get('db');
+		$hzad = self::oneOrNew($id);
 
-		if (empty($db))
+		if (empty($hzad->authenticator))
 		{
 			return false;
 		}
 
-		if (is_numeric($this->id))
-		{
-			$query = "INSERT INTO `#__auth_domain` (id,type,authenticator,domain,params) VALUES ( " . $db->quote($this->id) .
-				"," . $db->quote($this->type) . "," . $db->quote($this->authenticator) . "," . $db->quote($this->domain) . ","
-				. $db->quote($this->params) . ");";
-
-			$db->setQuery();
-
-			$result = $db->query();
-
-			if ($result !== false || $db->getErrorNum() == 1062)
-			{
-				return true;
-			}
-		}
-		else
-		{
-			$query = "INSERT INTO `#__auth_domain` (type,authenticator,domain,params) VALUES ( " .
-				$db->quote($this->type) . "," . $db->quote($this->authenticator) . "," . $db->quote($this->domain) . "," . $db->quote($this->params) . ");";
-
-			$db->setQuery($query);
-
-			$result = $db->query();
-			//var_dump($db);
-			if ($result === false && $db->getErrorNum() == 1062)
-			{
-				$query = "SELECT id FROM `#__auth_domain` WHERE authenticator=" .
-					$db->quote($this->authenticator) . " AND domain=" . $db->quote($this->domain) . " AND type=" . $db->quote($this->type) . ";";
-
-				$db->setQuery($query);
-
-				$result = $db->loadResult();
-
-				if ($result == null)
-				{
-					return false;
-				}
-
-				$this->id = $result;
-				return true;
-			}
-			else if ($result !== false)
-			{
-				$this->id = $db->insertid();
-				return true;
-			}
-		}
-
-		return false;
-	}
-
-	/**
-	 * Read a record
-	 *
-	 * @return  boolean  True on success, False on failure
-	 */
-	public function read()
-	{
-		$db = \App::get('db');
-
-		if (empty($db))
-		{
-			return false;
-		}
-
-		if (is_numeric($this->id))
-		{
-			$query = "SELECT id,type,authenticator,domain,params FROM `#__auth_domain` WHERE id=" .
-				$db->quote($this->id) . ";";
-		}
-		else
-		{
-			$query = "SELECT id,type,authenticator,domain,params FROM `#__auth_domain` WHERE "
-				. " type=" . $db->quote($this->type)
-				. " AND authenticator=" . $db->quote($this->authenticator)
-				. " AND domain=" . $db->quote($this->domain)
-				. ";";
-		}
-
-		$db->setQuery($query);
-
-		$result = $db->loadAssoc();
-
-		if (empty($result))
-		{
-			return false;
-		}
-
-		$this->clear();
-
-		foreach ($result as $key => $value)
-		{
-			$this->__set($key, $value);
-		}
-
-		$this->_updatedkeys = array();
-
-		return true;
-	}
-
-	/**
-	 * Update a record
-	 *
-	 * @param   boolean  $all  Update all values?
-	 * @return  boolean  True on success, False on failure
-	 */
-	public function update($all = false)
-	{
-		$db =  \App::get('db');
-
-		$query = "UPDATE `#__auth_domain` SET ";
-
-		$classvars = get_class_vars(__CLASS__);
-
-		$first = true;
-
-		foreach ($classvars as $property => $value)
-		{
-			if (($property{0} == '_'))
-			{
-				continue;
-			}
-
-			if (!$all && !in_array($property, $this->_updatedkeys))
-			{
-				continue;
-			}
-
-			if (!$first)
-			{
-				$query .= ',';
-			}
-			else
-			{
-				$first = false;
-			}
-
-			$value = $this->__get($property);
-
-			if ($value === null)
-			{
-				$query .= "`$property`=NULL";
-			}
-			else
-			{
-				$query .= "`$property`=" . $db->quote($value);
-			}
-		}
-
-		$query .= " WHERE `id`=" . $db->quote($this->__get('id')) . ";";
-
-		if ($first == true)
-		{
-			$query = '';
-		}
-
-		$db->setQuery($query);
-
-		if (!empty($query))
-		{
-			$result = $db->query();
-
-			if ($result === false)
-			{
-				return false;
-			}
-		}
-
-		return true;
-	}
-
-	/**
-	 * Delete a record
-	 *
-	 * @param   boolean  $deletelinks  Delete links?
-	 * @return  boolean  True on success, False on failure
-	 */
-	public function delete($deletelinks = false)
-	{
-		if (!isset($this->toolname) && !isset($this->id))
-		{
-			return false;
-		}
-
-		$db = \App::get('db');
-
-		if (empty($db))
-		{
-			return false;
-		}
-
-		if (!isset($this->id))
-		{
-			$db->setQuery("SELECT id FROM `#__auth_domain` WHERE `authenticator`=" . $db->quote($this->authenticator) . " AND `domain`=" . $db->quote($this->domain) . ";");
-
-			$this->id = $db->loadResult();
-		}
-
-		if (empty($this->id))
-		{
-			return false;
-		}
-
-		$db->setQuery("DELETE FROM `#__auth_domain` WHERE `id`= " . $db->quote($this->id) . ";");
-
-		if (!$db->query())
-		{
-			return false;
-		}
-
-		if ($deletelinks)
-		{
-			$db->setQuery("UPDATE `#__auth_links` SET `auth_domain_id`=NULL WHERE `auth_domain_id`=" . $db->quote($this->id) . ";");
-
-			$db->query();
-		}
-
-		return true;
-	}
-
-	/**
-	 * Retrieve the value of a property
-	 *
-	 * @param   string  $property  Property name
-	 * @return  mixed
-	 */
-	public function __get($property = null)
-	{
-		if (!property_exists(__CLASS__, $property) || $property{0} == '_')
-		{
-			if (empty($property))
-			{
-				$property = '(null)';
-			}
-
-			$this->_error("Cannot access property " . __CLASS__ . "::$" . $property, E_USER_ERROR);
-			die();
-		}
-
-		if (isset($this->$property))
-		{
-			return $this->$property;
-		}
-
-		if (array_key_exists($property, get_object_vars($this)))
-		{
-			return null;
-		}
-
-		$this->_error("Undefined property " . __CLASS__ . "::$" . $property, E_USER_NOTICE);
-
-		return null;
-	}
-
-	/**
-	 * Set a property
-	 *
-	 * @param   string  $property  Property name
-	 * @param   mixed   $value     Value to set
-	 * @return  void
-	 */
-	public function __set($property = null, $value = null)
-	{
-		if (!property_exists(__CLASS__, $property) || $property{0} == '_')
-		{
-			if (empty($property))
-			{
-				$property = '(null)';
-			}
-
-			$this->_error("Cannot access property " . __CLASS__ . "::$" . $property, E_USER_ERROR);
-			die();
-		}
-
-		$this->$property = $value;
-
-		if (!in_array($property, $this->_updatedkeys))
-		{
-			$this->_updatedkeys[] = $property;
-		}
-	}
-
-	/**
-	 * Check if a propety is set
-	 *
-	 * @param   string   $property  Property name
-	 * @return  boolean
-	 */
-	public function __isset($property = null)
-	{
-		if (!property_exists(__CLASS__, $property) || $property{0} == '_')
-		{
-			if (empty($property))
-			{
-				$property = '(null)';
-			}
-
-			$this->_error("Cannot access property " . __CLASS__ . "::$" . $property, E_USER_ERROR);
-			die();
-		}
-
-		return isset($this->$property);
-	}
-
-	/**
-	 * Unset a property
-	 *
-	 * @param   string  $property  Property name
-	 * @return  void
-	 */
-	public function __unset($property = null)
-	{
-		if (!property_exists(__CLASS__, $property) || $property{0} == '_')
-		{
-			if (empty($property))
-			{
-				$property = '(null)';
-			}
-
-			$this->_error("Cannot access property " . __CLASS__ . "::$" . $property, E_USER_ERROR);
-			die();
-		}
-
-		$this->_updatedkeys = array_diff($this->_updatedkeys, array($property));
-
-		unset($this->$property);
-	}
-
-	/**
-	 * Display an error
-	 *
-	 * @param   string   $message  Error message
-	 * @param   integer  $level    Error level
-	 * @return  void
-	 */
-	private function _error($message, $level = E_USER_NOTICE)
-	{
-		$caller = next(debug_backtrace());
-
-		switch ($level)
-		{
-			case E_USER_NOTICE:
-				echo "Notice: ";
-				break;
-			case E_USER_ERROR:
-				echo "Fatal error: ";
-				break;
-			default:
-				echo "Unknown error: ";
-				break;
-		}
-
-		echo $message . ' in ' . $caller['file'] . ' on line ' . $caller['line'] . "\n";
+		return $hzad;
 	}
 
 	/**
@@ -584,17 +282,32 @@ class Domain
 	 */
 	public static function find_or_create($type, $authenticator, $domain=null)
 	{
-		$hzad = new self();
-		$hzad->type = $type;
-		$hzad->authenticator = $authenticator;
-		$hzad->domain = $domain;
-		$hzad->read();
+		$query = self::all()
+			->whereEquals('type', $type)
+			->whereEquals('authenticator', $authenticator);
+		if ($domain)
+		{
+			$query->whereEquals('domain', $domain);
+		}
+		$row = $query->row();
 
-		if (empty($hzad->id) && !$hzad->create())
+		if (!$row || !$row->get('id'))
+		{
+			$row = self::blank();
+			$row->set('type', $type);
+			$row->set('authenticator', $authenticator);
+			if ($domain)
+			{
+				$row->set('domain', $domain);
+			}
+			$row->save();
+		}
+
+		if (!$row->get('id'))
 		{
 			return false;
 		}
 
-		return $hzad;
+		return $row;
 	}
 }
