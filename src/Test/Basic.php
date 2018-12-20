ab75155c51e89f9480b5098c6f5c827b4fd36c3d
Anthony Fuentes <anthony@anthonyfuentes.us>
2018-12-20 13:31:41 -0500

    [fix] Add test example to avoid no test warning (#507)

--- src/Test/Basic.php
+++ src/Test/Basic.php
@@ -38,4 +38,19 @@ namespace Hubzero\Test;
  */
 class Basic extends \PHPUnit_Framework_TestCase
 {
+
+	public function testExample()
+	{
+		// setup
+		$expected = 'example';
+		$systemUnderTest = new \stdClass();
+		$systemUnderTest->test = $expected;
+
+		// exercise
+		$actual = $systemUnderTest->test;
+
+		// verification
+		$this->assertEquals($expected, $actual);
+	}
+
 }
