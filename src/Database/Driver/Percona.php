2650ee3dfe97cfe0e9fd479ab4a07dbcf125c7ce
Shawn Rice <zooley@purdue.edu>
2019-03-22 07:57:33 -0400

    [feat] Adding Percona database driver Percona is a drop-in replacement for MySQL and supports all MySQL features but has a couple unique features of its own such as an engine type specific to it (xtradb).

---
+++ src/Database/Driver/Percona.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * HUBzero
+ *
+ * @package    framework
+ * @copyright  Copyright 2005-2019 HUBzero Foundation, LLC.
+ * @license    http://opensource.org/licenses/MIT MIT
+ * @since      Class available since release 2.2.15
+ */
+
+namespace Hubzero\Database\Driver;
+
+/**
+ * Percona (Pdo) database driver
+ *
+ * Percona is a drop-in replacement for MySQL and
+ * supports everything in the MySQL driver. But,
+ * it does have some features unique to it.
+ */
+class Percona extends Mysql
+{
+}
